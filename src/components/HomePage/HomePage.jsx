9edddd520349a6ebd21bd48dd1580e64120f9567
StergiosAnastasiadis <Stelioslonx@outlook.com>
2023-11-20 16:38:56 +0200

    add button to book cards

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { fetchAllBooks } from '../../services/apiCalls'
 import Card from 'react-bootstrap/Card';
+import { fetchAllBooks } from '../../services/apiCalls'
+import Button from 'react-bootstrap/Button';
 
 function HomePage() {
     const [books, setBooks] = useState([])
+
+
     const fetchBook = async () => {
         const res = await fetchAllBooks()
         if (res.error) {
@@ -23,13 +26,14 @@ function HomePage() {
             <div style={{ display: 'flex', flexWrap: 'wrap', gap: 15 }}>
                 {books?.map((book) => {
                     return (
-                        <Card style={{ width: '18rem' }}>
+                        <Card style={{ width: '18rem' }} key={book._id}>
                             <Card.Body>
                                 <Card.Title>{book.title}</Card.Title>
                                 <Card.Subtitle className="mb-2 text-muted">{book.author}</Card.Subtitle>
                                 <Card.Text>
                                     {book.price}
                                 </Card.Text>
+                                <Button variant="warning" onClick={() => alert(book._id)}>Edit</Button>
                             </Card.Body>
                         </Card>
                     )
